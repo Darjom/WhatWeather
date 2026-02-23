3716644ba4f6ac04f9e0619304cf8454ba452c9b
Daniel Jose Reque Mendoza <32781694+Darjom@users.noreply.github.com>
2026-02-23 09:41:12 -0400

    style: add initial styles for weather layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 
-import './App.css'
 
 function App() {
 
   return (
-    <>
-    <div>
-    <h1>Aplicacion en contruccion</h1>
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">WhatWeather ☀️</h1>
+
+      <button className="btn btn-success mt-3">
+        Buscar clima
+      </button>
     </div>
-    </>
   )
 }
 
